5ac4a8dfdfabbae62c81dc1e99cc74d60e66b80d
Alex Yamamura <acyamamu@uci.edu>
2019-07-06 14:55:50 -0700

    V9

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { Page2Component } from './page2/page2.component';
 import { Page1Component } from './page1/page1.component';
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
+  {path: 'dashboard', component: DashboardComponent},
   {path: 'page1', component: Page1Component},
   {path: 'page2', component: Page2Component},
 ];
---
+++ src/app/dashboard/friends.ts
@@ -0,0 +1,10 @@
+export class Friend{
+    name: string;
+    picture: string;
+    messages: string;
+    about: string;
+
+    rows: number;
+    columns: number;
+
+}
